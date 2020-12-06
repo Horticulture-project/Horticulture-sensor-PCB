3a9c58fee7e8a4e26596af85de0b9bcd1bbee1eb
Asbjørn Stokka <asbjorn.stokka@lyse.net>
2020-12-06 01:54:51 +0100

    Completed routing
    Soilsensor rev 1.0

--- stm32f030_horticulture_sensor/board.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2019 Antony Pavlov <antonynpavlov@gmail.com>
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-
